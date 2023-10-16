2ecfdb7fccc6138249330022dc19df1547edc735
Avantika <89470798+aprativ@users.noreply.github.com>
2023-10-15 21:30:42 -0400

    Update streamlit_app.py
    Added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title("Healthy diner")
 
-streamlit.text("Menu Details:")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard Boiled Free- Range Egg")
+
