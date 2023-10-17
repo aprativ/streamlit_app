486fc64f1b4b980d9ddcef101b7f8c1967602607
Avantika <89470798+aprativ@users.noreply.github.com>
2023-10-17 10:06:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit
 import pandas 
 import requests
-
-
-
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("Healthy diner")
 
@@ -39,9 +38,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 
-
-
-import snowflake.connector
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
