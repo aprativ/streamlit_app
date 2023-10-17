ad849cf42694497e26b89b811fb039b711cb428c
Avantika <89470798+aprativ@users.noreply.github.com>
2023-10-17 10:19:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,23 +35,20 @@ try:
 
 except URLError as e: 
   streamlit.error()
-  
-
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add','Kiwi')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+add_my_fruit)
-streamlit.write('Thanks for adding', add_my_fruit)
-my_cut.execute("insert into fruit_load_list_value('from streamlit')")
+#add_my_fruit = streamlit.text_input('What fruit would you like to add','Kiwi')
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+add_my_fruit)
+#streamlit.write('Thanks for adding', add_my_fruit)
+#my_cut.execute("insert into fruit_load_list_value('from streamlit')")
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-
+#streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
