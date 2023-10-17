6bfe216ed12a53c0e3bc1fcbcda25342dc857ed9
Avantika <89470798+aprativ@users.noreply.github.com>
2023-10-17 10:24:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ except URLError as e:
 # write your own comment - what does this do?
 #streamlit.dataframe(fruityvice_normalized)
 
-streamlit.stop()
+#streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
